4477a8a332783d8795303e4e42c68a3af3eb1cdf
Matheus Leal <matheus.eduardo.leal@gmail.com>
2026-01-17 20:43:14 -0300

    feat: implementando classes no dominio e adicionando camada de aplicacao

---
+++ dominio/venda.py
@@ -0,0 +1,18 @@
+import uuid
+
+class Venda():
+    def __init__(self, id_cliente, data_venda, id=str(uuid.uuid4())):
+        self.id = id
+        self.id_cliente = id_cliente
+        self.data_venda = data_venda
+
+    def to_dict(self):
+        return {
+            "id":self.id,
+            "id_cliente":self.id_cliente,
+            "data_venda":self.data_venda,
+        }
+    
+    @classmethod
+    def from_dict(cls, data):
+        return cls(uuid.UUID(data['id']), data['id_cliente'], data['data_venda'])
---
+++ dominio/venda_produto.py
@@ -0,0 +1,20 @@
+import uuid
+
+class VendaProduto():
+    def __init__(self, id_venda, id_produto, quantidade_produto, id=str(uuid.uuid4())):
+        self.id = id
+        self.id_venda = id_venda
+        self.id_produto = id_produto
+        self.quantidade_produto = quantidade_produto
+
+    def to_dict(self):
+        return {
+            "id":self.id,
+            "id_venda":self.id_venda,
+            "id_produto":self.id_produto,
+            "quantidade_produto":self.quantidade_produto,
+        }
+    
+    @classmethod
+    def from_dict(cls, data):
+        return cls(uuid.UUID(data['id']), data['id_venda'], data['id_produto'], data['quantidade_produto'])
