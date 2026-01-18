1f120a8dcbf4222f2c71fbed025ff9d033e95d95
Matheus Leal <matheus.eduardo.leal@gmail.com>
2026-01-18 12:49:31 -0300

    feat: adicionado atributos as entidades Venda e VendaProduto

--- dominio/venda.py
+++ dominio/venda.py
@@ -1,10 +1,11 @@
 import uuid
-
+from venda_produto import VendaProduto
 class Venda():
     def __init__(self, id_cliente, data_venda, id=str(uuid.uuid4())):
         self.id = id
         self.id_cliente = id_cliente
         self.data_venda = data_venda
+        self.produtos = list[VendaProduto] = []
 
     def to_dict(self):
         return {
